8eacee59d24c13430311a8096afca5cd6b8ad5cf
tetsu1615 <85781778+tetsu1615@users.noreply.github.com>
2023-05-19 22:13:34 +0900

    fix: delete-gitignore

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,10 +1,9 @@
-
 import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
     <>
-      <p className='text-center'>Next.js + TypeScript + SCSS + ESLint + Prettier + stylelint + Jest + React Testing Library + Cypress</p>
+      <p className='text-center text-blue-500'>Next.js + TypeScript + SCSS + ESLint + Prettier + stylelint + Jest + React Testing Library + Cypress</p>
     </>
   );
 };
