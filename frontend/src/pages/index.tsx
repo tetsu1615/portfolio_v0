49855835d041b4132f05b33e79a3b4b448ed650f
tetsu1615 <85781778+tetsu1615@users.noreply.github.com>
2023-05-19 23:10:51 +0900

    fix: style

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -2,8 +2,8 @@ import type { NextPage } from 'next';
 
 const Home: NextPage = () => {
   return (
-    <div className='min-h-full'>
-      <h1 className='text-3xl text-black text-center mt-20'>Ittetsu Takaki - Portfolio Site</h1>
+    <div className='min-h-full leading-7'>
+      <h1 className='text-3xl text-black text-center mt-20'>Ittetsu Takaki<br className='block sm:hidden' /> <span className='hidden sm:inline-block' >-</span> Portfolio Site</h1>
     </div>
   );
 };
