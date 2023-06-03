a5da23d94e3946cf8777e3718846eed49ff0d982
tetsu1615 <85781778+tetsu1615@users.noreply.github.com>
2023-06-03 14:59:53 +0900

    feat: wipページ作成

--- frontend/src/pages/_documents.tsx
+++ frontend/src/pages/_documents.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         {/*Google Fontsの読み込み*/}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;500;700;900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
